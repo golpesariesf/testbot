6351845e9ded65d4dcb32fbd3fc729b9d1006eca
Your Name <golpesariesf@gmail.com>
2024-03-05 11:00:27 +0300

    Add project files

--- index.py
+++ index.py
@@ -1,30 +1,27 @@
 ﻿import uuid
-from telegram import Bot, Update
+from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 # تنظیمات
-bot_token = "7137673728:AAE85wL1RBYskkrlCZaIzhEbgKmiEBiefDI"
-user_id = 5986365049
+bot_token = "YOUR_BOT_TOKEN"
 
 # تابع برای ساخت کد UUID
 def generate_uuid(update: Update, context: CallbackContext) -> None:
     unique_id = uuid.uuid4()
     unique_id_str = unique_id.hex
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f"UUID HEX: {unique_id_str}")
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"سلام! این کد UUID شماست: {unique_id_str}")
 
-# تابع برای ارسال کد به یک کاربر خاص
-def send_to_user(update: Update, context: CallbackContext) -> None:
-    unique_id = uuid.uuid4()
-    unique_id_str = unique_id.hex
-    context.bot.send_message(chat_id=user_id, text=f"New UUID HEX: {unique_id_str}")
+# تابع برای استارت ربات
+def start(update: Update, context: CallbackContext) -> None:
+    context.bot.send_message(chat_id=update.effective_chat.id, text="سلام! ربات شروع به کار کرد. برای دریافت کد UUID از دستور /generate_uuid استفاده کنید.")
 
 # ساخت ربات و اتصال به تلگرام
-updater = Updater(bot=Bot(token=bot_token), use_context=True)
+updater = Updater(token=bot_token, use_context=True)
 dispatcher = updater.dispatcher
 
 # تعریف دستورها
 dispatcher.add_handler(CommandHandler("generate_uuid", generate_uuid))
-dispatcher.add_handler(CommandHandler("send_to_user", send_to_user))
+dispatcher.add_handler(CommandHandler("start", start))
 
 # شروع گوش کردن به دستورات
 updater.start_polling()
